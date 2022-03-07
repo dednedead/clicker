3c28fdcfd272864a31b86ab7069c2a5ba15f1729
ded612 <100476091+dednedead@users.noreply.github.com>
2022-03-07 20:01:02 +0300

    sus

--- Control.gd
+++ Control.gd
@@ -1,27 +1,82 @@
 extends Control
 
-var coins:float = 1500
+var coins:float = 2000
+
 var knife_impr = 1
+var sharp_cost = 100
+
+var miniam1:bool = false
+var miniam1_time = 1.5
+
+var miniam2:bool = false
+var miniam2_time = 1.3
+
+var sus = 0.3
+var click_sus:float = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
-
-
 func _process(delta):
-	if coins>=30+knife_impr*70 and knife_impr < 5:
-		$Control/Button.disabled=false
+	$amogus/Panel/Label.text = str(coins)
+	$Control/Buttonsh/Label.text = "заточка ножа: "+str(sharp_cost)
+	
+	if coins>=sharp_cost and knife_impr <= 5:
+		$Control/Buttonsh.disabled=false
 	else:
-		$Control/Button.disabled=true
+		$Control/Buttonsh.disabled=true
+	
+	if knife_impr > 5:
+		$Control/Buttonsh/Label.text = "заточка ножа: -"
+	
+	if coins>=250 and miniam1 == false:
+		$Control/Buttonch.disabled=false
+	else:
+		$Control/Buttonch.disabled=true
+	
+	if coins>=500 and miniam2 == false:
+		$Control/Buttonch2.disabled=false
+	else:
+		$Control/Buttonch2.disabled=true
+	
+	if fmod(click_sus,35) == 0:
+		$amogus/Panel/TextureRect.visible = false
+		$amogus/Panel/TextureRect4.visible = true
+		$amogus/Panel/TextureRect4/Timer.start(sus)
+		click_sus += 1
 
+func _sus():
+	$amogus/Panel/TextureRect.visible = true
+	$amogus/Panel/TextureRect4.visible = false
+	$amogus/Panel/TextureRect4/Timer.stop()
+	
+func _knife_sharp():
+	coins -= sharp_cost
+	knife_impr += 1
+	sharp_cost = knife_impr*46+sharp_cost
 
 func _on_Button_pressed():
 	coins+=knife_impr
-	$amogus/Panel/Label.text = str(coins)
+	click_sus += 1
+	
+func _miniam1():
+	miniam1 = true
+	coins -= 250
+	$Control/Buttonch/Timer.start(miniam1_time)
+	$amogus/Panel/TextureRect2.visible = true
 
-func _knife_sharp():
-	coins -= 30+knife_impr*70
-	knife_impr += 1
+func _on_Timer1_timeout():
+	coins += 1
 
-		
+	
+func _miniam2():
+	miniam2 = true
+	coins -= 500
+	$Control/Buttonch2/Timer.start(miniam2_time)
+	$amogus/Panel/TextureRect3.visible = true
+
+func _on_Timer2_timeout():
+	coins += 2
+
+
